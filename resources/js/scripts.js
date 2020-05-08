7a673baae74a1a95b1ac3ebfe02beb9a4b701b50
Sahil Pant <sahilpant16@hotmail.com>
2020-05-08 18:43:31 +0530

    added Jquerry

---
+++ resources/js/scripts.js
@@ -0,0 +1,97 @@
+$(document).ready(function(){
+
+    /*FOR THE STICKY NAVIGATION*/
+    $('.js--section-features').waypoint(function(direction){
+
+        if(direction == "down"){
+            $('nav').addClass('sticky');
+        }
+        else{
+            $('nav').removeClass('sticky');
+        }
+
+    }, {
+        offset: '60px'
+    });
+
+
+    /*SCROLL ON BUTTONS*/
+
+
+    $('.js--scroll-to-plan').click(function(){
+
+        $('html,body').animate({scrollTop:$('.js--section-plans').offset().top},1000)
+
+    })
+
+    $('.js--scroll-to-start').click(function(){
+
+        $('html,body').animate({scrollTop:$('.js--section-features').offset().top},1000)
+
+    })
+
+
+    // Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+    /*ANIMATE ON SCROLL}*/
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animate__animated','animate__fadeIn','animate__delay-2s');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animate__animated','animate__fadeInUp','animate__delay-2s');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animate__animated','animate__fadeIn','animate__slower');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animate__animated','animate__pulse','animate__slower');
+    }, {
+        offset: '50%'
+    });
+
+
+});
