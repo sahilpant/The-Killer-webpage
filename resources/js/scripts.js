5581a0d2b9c1217d22edbd95a58e485c55e968bf
Sahil Pant <sahilpant16@hotmail.com>
2020-05-09 15:27:26 +0530

    added more jQuerry

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -70,28 +70,47 @@ $('a[href*="#"]')
 
     /*ANIMATE ON SCROLL}*/
     $('.js--wp-1').waypoint(function(direction) {
-        $('.js--wp-1').addClass('animate__animated','animate__fadeIn','animate__delay-2s');
+        $('.js--wp-1').addClass('animate__animated animate__fadeIn');
     }, {
         offset: '50%'
     });
 
     $('.js--wp-2').waypoint(function(direction) {
-        $('.js--wp-2').addClass('animate__animated','animate__fadeInUp','animate__delay-2s');
+        $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
     }, {
         offset: '50%'
     });
 
     $('.js--wp-3').waypoint(function(direction) {
-        $('.js--wp-3').addClass('animate__animated','animate__fadeIn','animate__slower');
+        $('.js--wp-3').addClass('animate__animated animate__fadeIn');
     }, {
         offset: '50%'
     });
 
     $('.js--wp-4').waypoint(function(direction) {
-        $('.js--wp-4').addClass('animate__animated','animate__pulse','animate__slower');
+        $('.js--wp-4').addClass('animate__animated animate__pulse');
     }, {
         offset: '50%'
     });
+
+
+    $('.js--nav-icon').click(function(){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i')
+
+        nav.slideToggle(200);
+        if(icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    })
+
+
+
+
 
 
 });
